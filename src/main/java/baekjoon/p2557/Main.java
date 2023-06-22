8cf3c4b57902794447bbfbfd6ac6f8df43818b88
paytakcode <paytakcode@gmail.com>
2023-06-22 23:24:08 +0900

    Feat: baekjoon-2557

---
+++ src/main/java/baekjoon/p2557/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.p2557;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+public class Main{
+
+	public void solution() throws IOException{
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		String str="Hello World!";
+		bw.write(str);
+		bw.close();
+	}
+
+	public static void main(String[] args) throws IOException{
+		new Main().solution();
+
+	}
+
+}
