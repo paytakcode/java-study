0e8f562bae17d8083e2c3673fe4ca80bd55931a7
paytakcode <paytakcode@gmail.com>
2023-06-24 20:27:25 +0900

    Feat: baekjoon-11382

---
+++ src/main/java/baekjoon/p11382/Main.java
@@ -0,0 +1,31 @@
+package baekjoon.p11382;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main{
+
+	public void solution() throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str);
+		BigInteger result = BigInteger.valueOf(0);
+		while (st.hasMoreTokens()) {
+			result = result.add(BigInteger.valueOf(Long.parseLong(st.nextToken())));
+		}
+		bw.write(result.toString());
+		bw.close();
+	}
+
+	public static void main(String[] args) throws IOException{
+		new Main().solution();
+
+	}
+
+}
