11a7e436fbc7094401215da3e28653cac2359eff
paytakcode <paytakcode@gmail.com>
2023-06-25 23:26:36 +0900

    Feat: baekjoon-25314

---
+++ src/main/java/baekjoon/p25314/Main.java
@@ -0,0 +1,25 @@
+package baekjoon.p25314;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public void solution() throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String result = "long ".repeat(N / 4) + "int";
+		bw.write(result);
+		bw.close();
+	}
+
+	public static void main(String[] args) throws IOException{
+		new Main().solution();
+
+	}
+
+}
