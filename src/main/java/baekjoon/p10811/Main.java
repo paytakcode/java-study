d87feaa7646df6861d97955b5f922c95799b1172
paytakcode <paytakcode@gmail.com>
2023-06-27 21:40:19 +0900

    Feat: baekjoon-10811

---
+++ src/main/java/baekjoon/p10811/Main.java
@@ -0,0 +1,49 @@
+package baekjoon.p10811;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public void solution() throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String nm = br.readLine();
+		StringTokenizer st = new StringTokenizer(nm);
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] baskets = new int[n];
+		for (int i = 0; i < n; i++){
+			baskets[i] = i + 1;
+		}
+		for (int t = 0; t < m; t++){
+			String ij = br.readLine();
+			st = new StringTokenizer(ij);
+			int i = Integer.parseInt(st.nextToken()) - 1;
+			int j = Integer.parseInt(st.nextToken()) - 1;
+			int k = (j - i) / 2;
+			for (int o = 0; o <= k; o++){
+				int temp = baskets[i + o];
+				baskets[i + o] = baskets[j - o];
+				baskets[j - o] = temp;
+			}
+		}
+		for (int basket : baskets) {
+			sb.append(basket).append(" ");
+		}
+
+		bw.write(sb.toString());
+		bw.close();
+	}
+
+	public static void main(String[] args) throws IOException{
+		new Main().solution();
+
+	}
+
+}
