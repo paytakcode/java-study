df3e491a5e287ed4f175193c255e8487948029ca
paytakcode <paytakcode@gmail.com>
2023-06-28 19:08:38 +0900

    Feat: baekjoon-27866

---
+++ src/main/java/baekjoon/p27866/Main.java
@@ -0,0 +1,12 @@
+package baekjoon.p27866;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.nextLine();
+		int i = input.nextInt();
+		System.out.println(str.charAt(i - 1));
+	}
+}
