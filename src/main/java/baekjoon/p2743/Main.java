6e98933abb5de5c4ed6b0de4b80f7089c8d03e4d
paytakcode <paytakcode@gmail.com>
2023-06-28 23:37:47 +0900

    Feat: baekjoon-2743

---
+++ src/main/java/baekjoon/p2743/Main.java
@@ -0,0 +1,11 @@
+package baekjoon.p2743;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String str = input.nextLine();
+		System.out.println(str.length());
+	}
+}
