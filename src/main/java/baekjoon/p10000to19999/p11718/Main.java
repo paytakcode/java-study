517912bbc298e52f768529bfda28d0ed11a54b50
paytakcode <paytakcode@gmail.com>
2023-06-29 15:02:09 +0900

    Feat: baekjoon-11718

---
+++ src/main/java/baekjoon/p10000to19999/p11718/Main.java
@@ -0,0 +1,21 @@
+package baekjoon.p10000to19999.p11718;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String str;
+		while ((str = br.readLine()) != null) {
+			bw.write(str);
+			bw.newLine();
+		}
+		br.close();
+		bw.close();
+	}
+}
