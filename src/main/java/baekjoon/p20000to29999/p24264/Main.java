dbba6a74f1858dfd711f362ffe8d13a365852945
paytakcode <paytakcode@gmail.com>
2023-07-10 18:52:38 +0900

    Feat: baekjoon-24264

---
+++ src/main/java/baekjoon/p20000to29999/p24264/Main.java
@@ -0,0 +1,20 @@
+package baekjoon.p20000to29999.p24264;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long n = Long.parseLong(br.readLine());
+		bw.write(String.valueOf(n * n));
+		bw.newLine();
+		bw.write("2");
+		br.close();
+		bw.close();
+	}
+}
